ec7fa33a75d11f9359f2b06b9639768f9d6a8cbe
othman853 <anuar.yasser@gmail.com>
2016-01-06 18:52:59 -0200

    Implementing fake repository

--- src/com/twu/biblioteca/model/FakeBookRepository.java
+++ src/com/twu/biblioteca/model/FakeBookRepository.java
@@ -1,8 +1,8 @@
 package com.twu.biblioteca.model;
 
-import com.twu.biblioteca.model.BookRepository;
 import com.twu.biblioteca.model.data.Book;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class FakeBookRepository implements BookRepository<Book> {
@@ -10,14 +10,32 @@ public class FakeBookRepository implements BookRepository<Book> {
     private List<Book> fakeBooks;
 
     public FakeBookRepository(List<Book> fakeBooks) {
+
         this.fakeBooks = fakeBooks;
     }
 
+    public FakeBookRepository() {
+        generateFakeBooks();
+    }
 
     @Override
     public List<Book> getAll() {
         return fakeBooks;
     }
 
+    private void generateFakeBooks() {
+        fakeBooks = new ArrayList<Book>();
+
+        for (int i = 0; i < 10; i++) {
+            Book generatedBook = new Book();
+            generatedBook.setCode(i + 1);
+            generatedBook.setName("Book " + (i + 1) );
+            generatedBook.setAuthor("Author " + (i + 1));
+            generatedBook.setAvailable(true);
+
+            fakeBooks.add(generatedBook);
+        }
+
+    }
 
 }
